9fafb027fbf08e713dbcffd27698f026925a1ff1
dzhang <debin.zhang@viasat.com>
2022-09-08 20:19:11 -0400

    add fix zscore scale

--- z_score/05_09_2022_ouyang_new_way/new_zscore_calculation.R
+++ z_score/05_09_2022_ouyang_new_way/new_zscore_calculation.R
@@ -39,6 +39,15 @@ zscore_plot <- function(data, feature, low=-3.0, high=3.0) {
   u + geom_boxplot()
 }
 
+# boxplot with fixed scale (zscore always displayed within (-3.0, 3.0) range)
+zscore_plot_fs <- function(data, feature, low=-3.0, high=3.0) {
+  t1<-adjustByMeanData(data, {{feature}})
+  t2<-getZScore(t1)
+  t3<-rmOutlier(t2, low, high)
+  u<-ggplot(t3, aes(x=Dataset, y=zscore))
+  u + geom_boxplot() + ylim(low, high)
+}
+
 # this section is for testing only
 t1<-adjustByMeanData(test_data, rh_bankssts_thickness)
 t2<-getZScore(t1)
@@ -55,12 +64,15 @@ u + geom_boxplot()
 
 zscore_plot(preharmo_data, rh_bankssts_thickness)
 zscore_plot(postharmo_data, rh_bankssts_thickness)
+zscore_plot_fs(postharmo_data, rh_bankssts_thickness)
 
 zscore_plot(preharmo_data, rh_caudalanteriorcingulate_thickness)
 zscore_plot(postharmo_data, rh_caudalanteriorcingulate_thickness)
+zscore_plot_fs(postharmo_data, rh_caudalanteriorcingulate_thickness)
 
 zscore_plot(preharmo_data, rh_caudalmiddlefrontal_thickness)
 zscore_plot(postharmo_data, rh_caudalmiddlefrontal_thickness)
+zscore_plot_fs(postharmo_data, rh_caudalmiddlefrontal_thickness)
 
 zscore_plot(preharmo_data, rh_cuneus_thickness)
 zscore_plot(postharmo_data, rh_cuneus_thickness)
