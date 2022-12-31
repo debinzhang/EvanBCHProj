e43ab456e487fe585940567add8014668fb40300
dzhang <debin.zhang@viasat.com>
2022-12-30 22:10:42 -0500

    combine R code into a single file harmonization.R

--- z_score/05_09_2022_ouyang_new_way/12_29_22_clean_build/harmonization.R
+++ z_score/05_09_2022_ouyang_new_way/12_29_22_clean_build/harmonization.R
@@ -10,10 +10,52 @@ library(reticulate)  # this package enable python in R
 
 use_python("/usr/local/bin/python3")
 
-wd <- "/Users/dzhang/Gits/EvanBCHProj/z_score/05_09_2022_ouyang_new_way/12_29_22_clean_build"
+wd <- getwd()
 setwd(wd)
 source_python("draw_shaded_plot.py")
 source_python("harmonDataBuilder_9_R_age_key.py")
+source_python("categorize_data.py")
+
+gen_raw_harmo_data <- function() {
+  file_list <- list("all_subjects_cortical_metrics_LH_curvind_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_LH_thicknessstd_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_RH_thickness_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_LH_foldind_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_RH_curvind_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_RH_thicknessstd_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_LH_gauscurv_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_RH_foldind_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_LH_meancurv_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_RH_gauscurv_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_LH_thickness_09_15_2022_preHarmo.csv",
+                    "all_subjects_cortical_metrics_RH_meancurv_09_15_2022_preHarmo.csv")
+  
+  for (file_path in file_list) {
+    print(paste("working on :", file_path, sep = ''))
+    file_name <- strsplit(file_path, "\\.")[[1]][1]
+    file_extension <- strsplit(file_path, "\\.")[[1]][2]
+    data_raw <- read.csv(file_path, stringsAsFactors = FALSE)
+    row_num_before <- nrow(data_raw)
+    
+    data_raw_1 <- data_raw[ which(data_raw$Age!="" & data_raw$Age!="Siemens"  &
+                                    (data_raw$Sex==1 | data_raw$Sex==2 |
+                                       data_raw$Sex=="M" | data_raw$Sex=="m" | data_raw$Sex=="F" |
+                                       data_raw$Sex=="f")
+    ),  ]
+    row_num_after_sex <- nrow(data_raw_1)
+    
+    outfile <- paste(file_name, '_clean', '.csv', sep="")
+    print(paste("creating cleanfile: ", outfile, sep = ''))
+    write.csv(data_raw_1, outfile, row.names = FALSE, quote = FALSE)
+    
+    # harmonize the outfile
+    harmo_file <- gsub("preHarmo", "postHarmo", outfile)
+    print(paste("creating harmo file: ", harmo_file, sep = ''))
+    print("------------")
+    harmonize(outfile, harmo_file, scanertype=FALSE, Eb=FALSE, Mean_only=TRUE, dataset=TRUE, Age=TRUE)
+  }
+}
+
 
 adjustByMeanData <- function(data, feature, byETIV=TRUE, IsLH=TRUE) {
   if (byETIV) {
@@ -1014,9 +1056,11 @@ process_rh_thickness <- function(draw_hemi_plot) {
   }
 }
 
-process_all <- function(draw_hemi_plot=TRUE) {
-  # process_lh_curvind()
-  # process_rh_curvind(draw_hemi_plot)
+process_all <- function(draw_hemi_plot=TRUE, clean_leftover=TRUE) {
+  gen_raw_harmo_data()
+  process_lh_curvind()
+  process_rh_curvind(draw_hemi_plot)
+
   # process_lh_foldind()
   # process_rh_foldind(draw_hemi_plot)
   # process_lh_gauscurv()
@@ -1025,10 +1069,10 @@ process_all <- function(draw_hemi_plot=TRUE) {
   # process_rh_meancurv(draw_hemi_plot)
   # process_lh_thicknessstd()
   # process_rh_thicknessstd(draw_hemi_plot)
-  process_lh_thickness()
-  process_rh_thickness(draw_hemi_plot)
+  # process_lh_thickness()
+  # process_rh_thickness(draw_hemi_plot)
+  categorize_data(clean_leftover)
 }
-
 
 
 #  The followings are test code 
